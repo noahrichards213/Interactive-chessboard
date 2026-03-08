d19cf130100edad42cad3e7dd2702e7c646b5a8e
noahrichards213 <noahzhengrichards@gmail.com>
2026-03-08 19:22:05 -0400

    Delete checkonedir.h

--- checkonedir.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHECKONEDIR
-#define CHECKONEDIR
-
-#include "constantsandstructs.h"
-
-// checks one direction, allows to check for rook, bishop and queen moves
-int* checkOneDir(Piece piece, int hort, int vert);
-
-#endif
