1f321b526fb129f8dc6956d7c296b764eace9e7f
Noah Richards <noahzhengrichards@gmail.com>
2026-05-29 14:27:40 -0400

    changes
    finished pseudcode structure for program

--- rev1/firmware_test/firmware_test.ino
+++ rev1/firmware_test/firmware_test.ino
@@ -7,9 +7,12 @@
 #define MISO_pin 13
 #define CLK_pin 12
 
-
 SPIClass *fspi = NULL;
-ICM42670 *IMU = NULL;
+MCP23S17 *MCP_1 = NULL;
+MCP23S17 *MCP_2 = NULL;
+
+int read_MCP(uint8_t index);
+void write_MCP(uint8_t index, uint8_t value);
 
 void setup()
 {
@@ -17,8 +20,8 @@ void setup()
   pinMode(CS_pin1, OUTPUT);
   pinMode(CS_pin2, OUTPUT);
 
-  MCP23S17 MCP_1(47, &fspi);
-  MCP23S17 MCP_2(48, &fspi)
+  MCP23S17 MCP_1(47, fspi);
+  MCP23S17 MCP_2(48, fspi);
 
   int rv = 0;
 
@@ -34,67 +37,81 @@ void setup()
   SPI.begin();
 
   //check to see if MCP object intializes properly
-  rv = MCP.begin();
-  Serial.println(rv ? "true" : "false");
+  rv = MCP_1.begin();
+  Serial.println(rv ? "true_1" : "false_1");
+
+  rv = MCP_2.begin();
+  Serial.println(rv ? "true_2" : "false_2");
+
 
   //check to see if we are using HWSPI (should be yes since we're not assigning pins?)
-  Serial.print("HWSPI: ");
-  Serial.println(MCP.usesHWSPI());
+  Serial.print("HWSPI_1: ");
+  Serial.println(MCP_1.usesHWSPI());
 
-  //will print true if successful
-  //0xFF means they all become INPUT (can read from it)
-  //0x00 means they all become OUTPUT (can write to it)
-  rv = MCP.pinMode8(0, 0x00);
+  Serial.print("HWSPI_2: ");
+  Serial.println(MCP_2.usesHWSPI());
+
+  //0xFF means 11111111 they all become INPUT (can read from it)
+  //0x00 means 00000000 they all become OUTPUT (can write to it)
+  
+  //we want hall effects to always be read mode and leds to be read mode
+  //0 is port A = write and 1 is port B = read
+
+  rv = MCP_1.pinMode8(0, 0x00);
   Serial.println(rv);
-  rv = MCP.pinMode8(1, 0x00);
+  rv = MCP_1.pinMode8(1, 0xFF);
   Serial.println(rv);
 
-  //writing laternating HIGH and LOWS to it
-  Serial.println("TEST write1(0)");
-  delay(100);
-  for (int i = 0; i < 16; i++)
-  {
-    MCP.write1(0, i % 2);  //  alternating HIGH/LOW
-    Serial.print(i % 2);
-    Serial.print(' ');
-    delay(100);
-  }
-  Serial.println();
-  Serial.println();
-
-  //setting to read mode
-  rv = MCP.pinMode8(0, 0xFF);
+  rv = MCP_2.pinMode8(0, 0x00);
   Serial.println(rv);
-  rv = MCP.pinMode8(1, 0xFF);
-  Serial.println(rv); 
-
-  //testing the read function
-  Serial.println("TEST read1(pin)");
-  for (int pin = 0; pin < 16; pin++)
-  {
-    int val = MCP.read1(pin);
-    Serial.print(val);
-    Serial.print(' ');
-    delay(100);
-  }
-  Serial.println();
+  rv = MCP_2.pinMode8(1, 0xFF);
+  Serial.println(rv);
 }
 
 
 void loop()
 {
-  delay(1000);
-  Serial.println("TEST read1(pin)");
-  for (int pin = 0; pin < 16; pin++)
-  {
-    int val = MCP.read1(pin);
-    Serial.print(val);
-    Serial.print(' ');
+  //testing all the LED's
+  for (int square = 0; square < 16; square++){
+    write_MCP(square, HIGH);
     delay(100);
+    write_MCP(square, LOW);
   }
-  Serial.println();
+
+  //testing hall effects
+  while (1){
+    for (int square = 0; square < 16; square++){
+      if (read_MCP(square) == HIGH){
+        write_MCP(square, HIGH);
+      } else if (read_MCP(square) == LOW){
+        write_MCP(square, LOW);
+      }
+
+    }
+  }
+
 
 }
 
+//index from 0 to 7 is portB
+//index from 8 - 15 is portA
+
+//based off where the hall effects are
+int read_MCP(uint8_t index){
+if (index < 8){
+ return MCP_1->read1(index);
+} else if (index > 8){
+ return MCP_2->read1(index - 8);
+}
+}
+
+//based off where the leds are
+void write_MCP(uint8_t index, uint8_t value){
+if (index < 8){
+  MCP_1->write1(index + 8, value);
+} else if (index > 8){
+  MCP_2->write1(index, value);
+}
+}
 
 //  -- END OF FILE --
