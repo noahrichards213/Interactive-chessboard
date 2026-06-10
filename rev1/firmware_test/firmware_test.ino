94f72dbe83b7596a77b85c1fa744f515de8fa848
Noah Richards <noahzhengrichards@gmail.com>
2026-06-10 15:39:12 -0400

    changes
    notes about plan for week of june 8

--- rev1/firmware_test/firmware_test.ino
+++ rev1/firmware_test/firmware_test.ino
@@ -2,116 +2,110 @@
 #include <SPI.h>
 
 #define CS_pin1 47
-#define CS_pin2 48
-#define MOSI_pin 11
-#define MISO_pin 13
 #define CLK_pin 12
+#define MISO_pin 13
+#define MOSI_pin 11
 
-SPIClass *fspi = NULL;
-MCP23S17 *MCP_1 = NULL;
-MCP23S17 *MCP_2 = NULL;
+// Define SPI and Device pointers
+SPIClass *fspi = nullptr;
+MCP23S17 *MCP_1 = nullptr;
 
-int read_MCP(uint8_t index);
-void write_MCP(uint8_t index, uint8_t value);
-
-void setup()
-{
-  fspi = new SPIClass(FSPI);
-  pinMode(CS_pin1, OUTPUT);
-  pinMode(CS_pin2, OUTPUT);
-
-  MCP23S17 MCP_1(47, fspi);
-  MCP23S17 MCP_2(48, fspi);
-
-  int rv = 0;
-
-  //prints information about the MCP23S17
+void setup() {
   Serial.begin(115200);
-  Serial.println();
-  Serial.println(__FILE__);
-  Serial.print("MCP23S17_LIB_VERSION: ");
-  Serial.println(MCP23S17_LIB_VERSION);
-  Serial.println();
-  delay(100);
 
-  SPI.begin();
-
-  //check to see if MCP object intializes properly
-  rv = MCP_1.begin();
-  Serial.println(rv ? "true_1" : "false_1");
-
-  rv = MCP_2.begin();
-  Serial.println(rv ? "true_2" : "false_2");
+  // 1. Initialize custom SPI bus
+  fspi = new SPIClass(FSPI);
+  fspi->begin(CLK_pin, MISO_pin, MOSI_pin, CS_pin1);
 
 
-  //check to see if we are using HWSPI (should be yes since we're not assigning pins?)
-  Serial.print("HWSPI_1: ");
-  Serial.println(MCP_1.usesHWSPI());
+  // 2. Initialize the MCP object
+  // Note: Ensure your library constructor allows passing an SPIClass reference.
+  // If your library only takes (CS, MOSI, MISO, CLK), it likely uses the default 'SPI' 
+  // object internally and cannot use your custom fspi bus.
+  MCP_1 = new MCP23S17(CS_pin1, fspi); 
 
-  Serial.print("HWSPI_2: ");
-  Serial.println(MCP_2.usesHWSPI());
+  // 4. Library Initialization
+  if (MCP_1->begin(true) == false) {
+    Serial.println("Library begin() failed.");
+  } else {
+    // 5. Setup configuration
+    MCP_1->pinMode8(0, 0x00); // Port A as Output
+    // MCP_1->pinMode8(1, 0xFF); // Port B as Input
+    Serial.println("Configuration complete.");
 
-  //0xFF means 11111111 they all become INPUT (can read from it)
-  //0x00 means 00000000 they all become OUTPUT (can write to it)
-  
-  //we want hall effects to always be read mode and leds to be read mode
-  //0 is port A = write and 1 is port B = read
+  // MCP_1->pinMode1(11, OUTPUT);
+  // MCP_1->write1(11, HIGH);
+  // MCP_1->pinMode1(8, INPUT);
 
-  rv = MCP_1.pinMode8(0, 0x00);
-  Serial.println(rv);
-  rv = MCP_1.pinMode8(1, 0xFF);
-  Serial.println(rv);
+  // MCP_1->pinMode1(8, INPUT_PULLUP);
 
-  rv = MCP_2.pinMode8(0, 0x00);
-  Serial.println(rv);
-  rv = MCP_2.pinMode8(1, 0xFF);
-  Serial.println(rv);
+
+
+  }
 }
-
 
 void loop()
 {
+  MCP_1->write1(7, HIGH);
+  delay(1300);
+  MCP_1->write1(7, LOW);
+  delay(1300);
+
+  int value1 = MCP_1->read1(8);
+  Serial.print("The value1 is ");
+  Serial.println(value1);
+
+  int value2 = MCP_1->read1(9);
+  Serial.print("The value2 is ");
+  Serial.println(value2);
+
+
+  // MCP_1->write1(7, LOW);
+  // delay(5000);
+  // Serial.println("its low");
   //testing all the LED's
-  for (int square = 0; square < 16; square++){
-    write_MCP(square, HIGH);
-    delay(100);
-    write_MCP(square, LOW);
-  }
+  // for (int square = 0; square < 16; square++){
+  //   Serial.print("We are on square: ");
+  //   Serial.print(square);
+  //   write_MCP(square, HIGH, MCP_1, MCP_2);
+  //   delay(100);
+  //   write_MCP(square, LOW, MCP_1, MCP_2);
+  // }
 
   //testing hall effects
-  while (1){
-    for (int square = 0; square < 16; square++){
-      if (read_MCP(square) == HIGH){
-        write_MCP(square, HIGH);
-      } else if (read_MCP(square) == LOW){
-        write_MCP(square, LOW);
-      }
+  // while (1){
+  //   for (int square = 0; square < 16; square++){
+  //     if (read_MCP(square) == HIGH){
+  //       write_MCP(square, HIGH);
+  //     } else if (read_MCP(square) == LOW){
+  //       write_MCP(square, LOW);
+  //     }
 
-    }
-  }
+  //   }
+  // }
 
 
 }
 
-//index from 0 to 7 is portB
-//index from 8 - 15 is portA
+//index from 0 to 7 is portA
+//index from 8 - 15 is portB
 
 //based off where the hall effects are
-int read_MCP(uint8_t index){
-if (index < 8){
- return MCP_1->read1(index);
-} else if (index > 8){
- return MCP_2->read1(index - 8);
-}
-}
+// int read_MCP(uint8_t index, MCP23S17* MCP_1, MCP23S17* MCP_2){
+// if (index < 8){
+//  return MCP_1->read1(index + 8);
+// } else if (index >= 8){
+//  return MCP_2->read1(index);
+// }
+// }
 
-//based off where the leds are
-void write_MCP(uint8_t index, uint8_t value){
-if (index < 8){
-  MCP_1->write1(index + 8, value);
-} else if (index > 8){
-  MCP_2->write1(index, value);
-}
-}
+// //based off where the leds are
+// void write_MCP(uint8_t index, uint8_t value, MCP23S17* MCP_1, MCP23S17* MCP_2){
+// if (index < 8){
+//   MCP_1->write1(index, value);
+// } else if (index >= 8){
+//   MCP_2->write1(index - 8, value);
+// }
+// }
 
 //  -- END OF FILE --
