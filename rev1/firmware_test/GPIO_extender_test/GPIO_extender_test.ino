9328ce52676ec010acd30b88ca1fa5e3055ba06b
Noah Richards <noahzhengrichards@gmail.com>
2026-07-02 23:12:05 -0400

    updating notes

--- rev1/firmware_test/GPIO_extender_test/GPIO_extender_test.ino
+++ rev1/firmware_test/GPIO_extender_test/GPIO_extender_test.ino
@@ -1,66 +1,91 @@
-#include "MCP23S17.h"
-#include <SPI.h>
+  #include "MCP23S17.h"
+  #include <SPI.h>
 
-#define CS_pin1 47
-#define CLK_pin 12
-#define MISO_pin 13
-#define MOSI_pin 11
+  #define CS_pin1 47
+  #define CS_pin2 48
+  #define CLK_pin 12
+  #define MISO_pin 13
+  #define MOSI_pin 11
 
-SPIClass *fspi = nullptr;
-MCP23S17 *MCP_1 = nullptr;
+  SPIClass *fspi_1 = nullptr;
+  MCP23S17 *MCP_1 = nullptr;
+  MCP23S17 *MCP_2 = nullptr;
 
-void setup() {
-  Serial.begin(115200);
+  void setup() {
+    Serial.begin(115200);
 
-  fspi = new SPIClass(FSPI);
-  fspi->begin(CLK_pin, MISO_pin, MOSI_pin, CS_pin1);
+    fspi_1 = new SPIClass(FSPI);
+    fspi_1->begin(CLK_pin, MISO_pin, MOSI_pin, CS_pin1);
 
-  MCP_1 = new MCP23S17(CS_pin1, fspi); 
+    MCP_1 = new MCP23S17(CS_pin1, fspi_1); 
+    MCP_2 = new MCP23S17(CS_pin2, fspi_1);
 
-  if (MCP_1->begin() == false) {
-    Serial.println("Library begin() failed.");
-  } else {
-      MCP_1->pinMode8(0, 0x00); 
-      MCP_1->pinMode8(1, 0xFF); 
-    Serial.println("Configuration complete.");
+    if (MCP_1->begin() == false) {
+      Serial.println("Library begin1() failed.");
+    } else {
+        MCP_1->pinMode8(0, 0x00); 
+        MCP_1->pinMode8(1, 0xFF); 
+      Serial.println("Configuration 1 complete.");
+
+    }
+
+    if (MCP_2->begin() == false) {
+      Serial.println("Library begin2() failed.");
+    } else {
+        MCP_2->pinMode8(0, 0x00); 
+        MCP_2->pinMode8(1, 0xFF); 
+      Serial.println("Configuration 2 complete.");
+
+    }
+  }
+
+  void loop()
+  {
+    MCP_1->write1(7, HIGH);
+    MCP_1->write1(6, HIGH);
+
+    MCP_2->write1(6, HIGH);
+    MCP_2->write1(5, HIGH);
+
+    delay(1000);
+
+    MCP_1->write1(7, LOW);
+    MCP_1->write1(6, LOW);
+
+    MCP_2->write1(6, LOW);
+    MCP_2->write1(5, LOW);
+
+    delay(1000);
+    // MCP_1->write1(6, LOW);
+    // MCP_1->write1(5, LOW);
+
+    // delay(1300);
+
+
+    // MCP_1->write1(7, LOW);
+    // delay(5000);
+    // Serial.println("its low");
+    //testing all the LED's
+    // for (int square = 0; square < 16; square++){
+    //   Serial.print("We are on square: ");
+    //   Serial.print(square);
+    //   write_MCP(square, HIGH, MCP_1, MCP_2);
+    //   delay(100);
+    //   write_MCP(square, LOW, MCP_1, MCP_2);
+    // }
+
+    //testing hall effects
+    // while (1){
+    //   for (int square = 0; square < 16; square++){
+    //     if (read_MCP(square) == HIGH){
+    //       write_MCP(square, HIGH);
+    //     } else if (read_MCP(square) == LOW){
+    //       write_MCP(square, LOW);
+    //     }
+
+    //   }
+    // }
+
 
   }
-}
-
-void loop()
-{
-  MCP_1->write1(7, HIGH);
-
-  delay(1300);
-  MCP_1->write1(7, LOW);
-
-  delay(1300);
-
-
-  // MCP_1->write1(7, LOW);
-  // delay(5000);
-  // Serial.println("its low");
-  //testing all the LED's
-  // for (int square = 0; square < 16; square++){
-  //   Serial.print("We are on square: ");
-  //   Serial.print(square);
-  //   write_MCP(square, HIGH, MCP_1, MCP_2);
-  //   delay(100);
-  //   write_MCP(square, LOW, MCP_1, MCP_2);
-  // }
-
-  //testing hall effects
-  // while (1){
-  //   for (int square = 0; square < 16; square++){
-  //     if (read_MCP(square) == HIGH){
-  //       write_MCP(square, HIGH);
-  //     } else if (read_MCP(square) == LOW){
-  //       write_MCP(square, LOW);
-  //     }
-
-  //   }
-  // }
-
-
-}
 
--- rev1/firmware_test/multiplexer_code/Multiplexer_code/Multiplexer_code.ino
+++ rev1/firmware_test/multiplexer_code/Multiplexer_code/Multiplexer_code.ino
@@ -6,27 +6,49 @@
 #define S2 40
 #define S3 41
 
-//this will be connected to a hall effect
-#define channel_num 0
-
 const int g_common_pin = 3;
+int channel_num = 0;
 
-CD74HC4067 mux_A(S0, S1, S2, S3);
+CD74HC4067 mux_A(S0, -1, -1, -1);
 
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("The ESP can print");
+  pinMode(S0, OUTPUT);
 }
 
 void loop() {
   pinMode(g_common_pin, INPUT);
   int sensorValue;
+
+  if (channel_num == 1){
+    channel_num = 0;
+    digitalWrite(S0, 0);
+  } else {
+    channel_num = 1;
+    digitalWrite(S0, 1);
+  }
+
+
   mux_A.channel(channel_num);
   sensorValue = analogRead(g_common_pin);
 
-  Serial.print("This is the pin value from channel ");
-  Serial.println(channel_num);
-  Serial.print(sensorValue);
+  Serial.print("Piece ");
+  Serial.print(channel_num);
+  Serial.print(" ");
+  if (sensorValue > 500){
+    Serial.println("The piece is off");
+  } else {
+    Serial.println("The piece is on");
+  }
+
+  Serial.print("The sensor value is: ");
+  Serial.println(sensorValue);
+
+
+  // Serial.print("This is the pin value from channel: ");
+  // Serial.print(channel_num);
+  // Serial.println(sensorValue);
 }
